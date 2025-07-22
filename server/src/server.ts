ebac7720505642e0bb4d545143d76d556af44bb6
gerismumo <geraldmumo6@gmail.com>
2025-07-22 04:59:15 +0300

    init setup of dependencies

---
+++ server/src/server.ts
@@ -0,0 +1,11 @@
+import * as dotenv from "dotenv";
+dotenv.config({ path: ".env" });
+
+import app from "./app";
+
+const HOST_NAME = process.env.HOST_NAME || "0.0.0.0";
+const PORT = parseInt(process.env.PORT || "3000", 10);
+
+app.listen(PORT, HOST_NAME, () => {
+  console.log(`Server is running on http://${HOST_NAME}:${PORT}`);
+});
